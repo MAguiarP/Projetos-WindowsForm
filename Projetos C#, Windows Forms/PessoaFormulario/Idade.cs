604a2b53b0d23d3f0fab44bbaef8039a9515842c
MAguiarP <matheusdeaguiar@live.com>
2022-09-20 10:38:47 -0300

    up

---
+++ Projetos C#, Windows Forms/PessoaFormulario/Idade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PessoaFormulario
+{
+    internal class Idade
+    {
+        public string CalcularIdade(int IntDia, int IntMes, int IntAno)
+        {
+            string StrMensagem = string.Empty;
+
+            DateTime dataNascimento = new DateTime(IntAno, IntMes, IntDia);
+
+            TimeSpan idade = DateTime.Now - dataNascimento;
+            int anos = Convert.ToInt32(idade.Days / 365 );
+            int meses = Convert.ToInt32(idade.Days % 365 / 30 );
+            int dias = Convert.ToInt32(idade.Days % 365 % 30 );
+
+            StrMensagem = "Tem " + anos + "Anos " + meses + "Meses e " + dias + "dias de vida ";
+            return StrMensagem;
+        }
+        public string CalcularIdade(int IntAno, int IntMes)
+        {
+            string StrMensagem = string.Empty;
+            DateTime DataNascimento = new DateTime(IntAno, IntMes, 30);
+            TimeSpan Idade = DateTime.Now - DataNascimento;
+            int Anos = Convert.ToInt32(Idade.Days / 365);
+
+            StrMensagem = "tem " + Anos + " anos de vida ";
+
+            return StrMensagem;
+        }
+    }
+}
---
+++ Projetos C#, Windows Forms/PessoaFormulario/Validar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PessoaFormulario
+{
+    internal class Validar
+    {
+        public string ClassificarQIeCalcularIdadePessoa(int IntQI, int IntAno, int IntMes)
+        {
+            Idade ObjIdade = new Idade();
+            string StrMensagem = string.Empty;
+            StrMensagem = ClassificarQIeCalcularIdadePessoa(IntQI);
+            StrMensagem += ObjIdade.CalcularIdade(IntAno, IntMes);
+
+            return StrMensagem;
+        }
+    }
+}
